9bb7d68d49d3da3685081f309957fd3f3deffe2d
air <sarnor999@gmail.com>
2023-09-24 09:40:12 +0400

    first commit1

---
+++ server.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const express = require("express");
+const fs = require("fs");
+
+
+
+
+const app = express();
+
+app.use(express.json());
+app.use(
+    express.static(path.join(__dirname, `/`), {
+        index: "none",
+    })
+);
+app.get("/", (req, res) => {
+    fs.readFile(
+        path.resolve(__dirname, "index.html"),
+        "utf8",
+        (err, html) => {
+            if (err) {
+                throw (err)
+            } else {
+                res.send(html)
+            }
+        }
+    );
+});
+
+app.listen(80, () => console.log('Server Run'));
